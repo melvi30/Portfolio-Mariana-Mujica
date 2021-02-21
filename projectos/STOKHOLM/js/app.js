39878fb3d4b3ab5e9a60cc8e53b7fa4f20e3f415
Mariana Mujica Gonzalez <78142719+melvi30@users.noreply.github.com>
2021-02-21 22:59:39 +0100

    projectos-stk

---
+++ projectos/STOKHOLM/js/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+// Cuando hago click en "fas" le añado "ver" a "nav"
+
+const fas = document.querySelector(".fas")
+const menu = document.querySelector(".menu")
+
+    fas.addEventListener("click",function(){
+        menu.classList.toggle("ver")
+    })
+
