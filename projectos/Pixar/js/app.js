51946931d68736bcdca9c69cd1ecd43e19584b3d
melvi30 <marianamujica30@gmail.com>
2021-02-26 12:52:40 +0100

    cambios

--- projectos/Pixar/js/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Cuando hago click en "fas fab-bar" añado ver a "menu" - version pc //
-var fas = document.querySelector(".fas");
-var menu = document.querySelector(".menu");
-var li = document.querySelector(".li");
-fas.addEventListener("click", function () {
-    menu.classList.toggle("ver");
-});
--- projectos/STOKHOLM/js/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-// Cuando hago click en "fas" le añado "ver" a "nav"
-var fas = document.querySelector(".fas");
-var menu = document.querySelector(".menu");
-fas.addEventListener("click", function () {
-    menu.classList.toggle("ver");
-});
